4e1b16b82b80ca650bafe9e8629b9526f8d06e5b
Benjamin Trounson <btrounson@outlook.co.nz>
2023-01-26 11:35:40 +1300

    Initial Commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import SelectDropdown from './components/SelectDropdown';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <SelectDropdown/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/SelectDropdown.tsx
@@ -0,0 +1,9 @@
+import {View} from "react-native";
+
+export default function SelectDropdown() {
+    return (
+        <View>
+
+        </View>
+    )
+}
